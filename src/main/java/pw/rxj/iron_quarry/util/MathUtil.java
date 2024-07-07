4dd3f7cf679d075463b98446677b93111988904f
RXJpaw <github@rxj.pw>
2024-07-07 09:00:30 +0200

    ⚡️ Introduced LOG2 as a static constant.

--- src/main/java/pw/rxj/iron_quarry/util/MathUtil.java
+++ src/main/java/pw/rxj/iron_quarry/util/MathUtil.java
@@ -1,7 +1,9 @@
 package pw.rxj.iron_quarry.util;
 
 public class MathUtil {
+    public static final double LOG2 = Math.log(2);
+
     public static double log2(double a) {
-        return Math.log(a) / Math.log(2);
+        return Math.log(a) / LOG2;
     }
 }
